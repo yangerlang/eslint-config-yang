28d77e83d9ccfdbc73066a2f2ee94ccb3d283e3d
xiaohebei <yangerlanginfo@gmail.com>
2021-06-12 23:26:31 +0800

    add file

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: 'airbnb-base',
+  rules: {
+    // 缩进
+    indent: ['error', 2, {switchcase: 1}],
+    // 不强制尾逗号
+    'no-underscore-dangle': 'off',
+    'comma-dangle': 'off'
+  }
+}
+
